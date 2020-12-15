4addffbf30b6d7ee95ea99e42ee7b78517023147
OyugoObonyo <nyikal1999@gmail.com>
2020-12-15 17:13:49 +0000

    Complete tasks

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/bin/usr/python3
+def multiple_returns(sentence):
+    slen = len(sentence)
+    fchar = sentence[0] if slen > 0 else None
+
+    return (slen, fchar)
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    m = my_list[0]
+    for x in my_list:
+        if x > m:
+            m = x
+
+    return m
