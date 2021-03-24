9b61770207f1691d8c67f83291b23390c66bbccf
OyugoObonyo <nyikal1999@gmail.com>
2021-03-24 16:08:09 +0300

    Prints first arg passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 // Prints first arguement passed to it
 
-const arguements = process.argv;
-if (arguements[2]) {
-  console.log(arguements[2]);
-} else {
-  console.log('No arguement');
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
