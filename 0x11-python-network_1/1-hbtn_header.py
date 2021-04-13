79e08944a1fa4d78d4e42c78bd17b61a21c949ab
OyugoObonyo <nyikal1999@gmail.com>
2021-04-13 15:29:10 +0300

    Make executable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,11 +3,12 @@
 Takes in a URL, sends request to that URL and displays
 value of the X-Request-Id variable found in the header of the response.
 """
+
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as reponse:
-    for header in reponse.getheaders():
-        if header[0] == 'X-Request-Id':
-            print(header[1])
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        requestId = response.getheader("X-Request-Id")
+        print(requestId)
