de67522a4cbef5f5e836b907d3c721907f57d3ee
OyugoObonyo <nyikal1999@gmail.com>
2021-04-13 15:16:05 +0300

    Fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
