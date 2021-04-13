83267dea90a90565fce9b264b830d8cc4f6e9ffe
OyugoObonyo <nyikal1999@gmail.com>
2021-04-13 15:59:19 +0300

    Change arg index

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,11 +9,11 @@ import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[2]
-values = {'email': sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    the_page = response.read().decode('utf-8')
-    print(the_page)
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
+    
