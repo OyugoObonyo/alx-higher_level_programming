1af709fe3a004400d2f25f31dcec0e9154d00400
OyugoObonyo <nyikal1999@gmail.com>
2021-04-13 11:57:06 +0300

    Fetch https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+url = 'https://intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode("utf-8")))
