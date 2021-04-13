667435f827e6fad5f5972ee4576ed664ec97f0b8
OyugoObonyo <nyikal1999@gmail.com>
2021-04-13 16:02:18 +0300

    Fix:pep8 errors

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -16,4 +16,3 @@ if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
-    
