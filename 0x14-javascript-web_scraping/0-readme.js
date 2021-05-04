518520281d94b8cd1621a0f440ad0a04e1d325ed
OyugoObonyo <nyikal1999@gmail.com>
2021-05-04 16:49:10 +0300

    Fix: add else condition

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,5 +5,7 @@ const args = process.argv;
 fs.readFile(args[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
-  } console.log(data);
+  } else {
+    console.log(data);
+  }
 });
