a4445f5465f10553eefe318d6af9d0c90640f9b5
OyugoObonyo <nyikal1999@gmail.com>
2021-05-19 17:26:26 +0300

    Push all files

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+    success: function (data) {
+      const results = data.results;
+      for (const result of results) {
+        $('UL#list_movies').append('<li>' + result.title + '</li>');
+      }
+    }
+  });
+});
+
